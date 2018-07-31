f2a87eea625428916ff46b656084b79daefd0cf8
Your Name <you@example.com>
2018-07-31 23:22:21 +0500

    deploy

--- twitter/settings.py
+++ twitter/settings.py
@@ -119,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
